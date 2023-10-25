b67f5e767483bce118e0859b74c369b1b2dc9b12
SelenaIfy <princess01selena@gmail.com>
2023-10-25 23:10:27 +0000

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Square - A class Square that defines a square"""
+    def __init__(self, size):
+        """__init__ - defines the attribute of an instance
+
+        Args:
+            self: A reference to the object instance
+            size: size of the square
+
+        Return:
+            None
+        """
+        self.__size = size
